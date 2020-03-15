ce02c5c66cfca9b31ba9bd5889129e3234983f75
ramya <ramyaiyer96@gmail.com>
2020-03-15 17:29:52 +0530

    changes with API

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 const mapStyles = {
-  width: '100%',
-  height: '100%'
+  width: "100%",
+  height: "100%"
 };
 class GoogleMap extends React.Component {
   render() {
-    const { lat, lng } = this.props;
+    const { lat, lng, stations } = this.props;
     return (
       <Map
         google={this.props.google}
-        zoom={8}
+        zoom={5}
         style={mapStyles}
         center={{ lat, lng }}
       >
-        <Marker position={{ lat, lng }} />
+        {/*<Marker position={{ lat, lng }} />*/}
+        {stations.map((station, index) => {
+          return (
+            <Marker
+              key={index}
+              name={station.name}
+              position={{ lat: station.lat, lng: station.lng }}
+            />
+          );
+        })}
       </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDB0ZGHZHLI4o5Bqjmbi_VJ0MwBApV-Gd0'
+  apiKey: process.env.REACT_APP_MAP_KEY
 })(GoogleMap);
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,132 +1,186 @@
-import React from 'react';
-import { Row, Col, Select, Divider } from 'antd';
-import GoogleMap from '../GoogleMap';
-import './style.css';
-import { getCountries, getStates, getCities } from '../../helpers';
+import React from "react";
+import { Row, Col, Select, Divider, Spin } from "antd";
+import axios from "axios";
+import GoogleMap from "../GoogleMap";
+import "./style.css";
+import { getCountries, getStates, getCities } from "../../helpers";
 const { Option } = Select;
-
+const BASE_URL = "http://127.0.0.1:5000";
 export default class Map extends React.Component {
-	state = {
-		countries: [],
-		states: [],
-		cities: [],
-		latitude: 42.317432,
-		longitude: -83.026772
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			countries: [],
+			states: [],
+			cities: [],
+			latitude: 42.317432,
+			longitude: -83.026772,
+			stations: [],
+			loading: false
+		};
+	}
 
 	componentDidMount() {
 		const countries = getCountries();
 		this.setState({ countries });
 	}
 
-	countryChanged = countryId => {
+	countryChanged = async countryId => {
 		const { countries } = this.state;
 		let countryDetails = countries.find(
 			each => each.countryID === countryId
 		);
 		const { latitude, longitude } = countryDetails;
+		// const stations = await this.getByLatLong(latitude, longitude);
+		const stations = await this.getByName(countryDetails.countryName);
 		const states = getStates(countryId);
-		this.setState({ states, latitude, longitude });
+		this.setState({ states, latitude, longitude, stations });
 	};
 
-	stateChanged = stateId => {
+	stateChanged = async stateId => {
 		const { states } = this.state;
 		let stateDetails = states.find(each => each.stateID === stateId);
 		const { latitude, longitude } = stateDetails;
+		// const stations = await this.getByLatLong(latitude, longitude);
+		const stations = await this.getByName(stateDetails.stateName);
 		const cities = getCities(stateId);
-		this.setState({ cities, latitude, longitude });
+		this.setState({ cities, latitude, longitude, stations });
 	};
 
-	citySelected = cityId => {
+	citySelected = async cityId => {
 		const { cities } = this.state;
 		let cityDetails = cities.find(each => each.cityID === cityId);
 		const { latitude, longitude } = cityDetails;
+		// const stations = await this.getByLatLong(latitude, longitude);
+		const stations = await this.getByName(cityDetails.cityName);
 		this.setState({
 			latitude,
-			longitude
+			longitude,
+			stations
 		});
 	};
 
-	handleLoaded = (map, maps) => {
-		console.log(map, 'mapmapmap');
-		console.log(maps, 'mapsmapsmaps');
+	getByLatLong = async (lat, lng) => {
+		try {
+			await this.setState({
+				loading: true
+			});
+			let formData = new FormData();
+			formData.append("lat", lat);
+			formData.append("lng", lng);
+			let data = await axios.post(`${BASE_URL}/reverse`, formData);
+			await this.setState({
+				loading: false
+			});
+			return data.data;
+		} catch (err) {
+			return [];
+		}
+	};
+
+	getByName = async name => {
+		try {
+			await this.setState({
+				loading: true
+			});
+			let data = await axios.get(`${BASE_URL}/${name}`);
+			await this.setState({
+				loading: false
+			});
+			return data.data;
+		} catch (err) {
+			await this.setState({
+				loading:false
+			})
+			return [];
+		}
 	};
 
 	render() {
-		const { countries, states, cities, latitude, longitude } = this.state;
+		const {
+			countries,
+			states,
+			cities,
+			latitude,
+			longitude,
+			loading,
+			stations
+		} = this.state;
 		return (
 			<React.Fragment>
-				<Row>
-					<Col span={8}>
-						<Select
-							showSearch
-							style={{ width: 200 }}
-							placeholder="Select Country"
-							optionFilterProp="children"
-							autoComplete="off"
-							onChange={this.countryChanged}
-							filterOption={(input, option) =>
-								option.children
-									.toLowerCase()
-									.indexOf(input.toLowerCase()) >= 0
-							}
-						>
-							{countries.map(country => (
-								<Option key={country.countryID}>
-									{country.countryName}
-								</Option>
-							))}
-						</Select>
-					</Col>
-					<Col span={8}>
-						<Select
-							showSearch
-							style={{ width: 200 }}
-							placeholder="Select State"
-							optionFilterProp="children"
-							onChange={this.stateChanged}
-							filterOption={(input, option) =>
-								option.children
-									.toLowerCase()
-									.indexOf(input.toLowerCase()) >= 0
-							}
-						>
-							{states.map(state => (
-								<Option key={state.stateID}>
-									{state.stateName}
-								</Option>
-							))}
-						</Select>
-					</Col>
-					<Col span={8}>
-						<Select
-							style={{ width: 200 }}
-							placeholder="Select City"
-							optionFilterProp="children"
-							onChange={this.citySelected}
-							filterOption={(input, option) =>
-								option.children
-									.toLowerCase()
-									.indexOf(input.toLowerCase()) >= 0
-							}
-						>
-							{cities.map(city => (
-								<Option
-									key={`${city.cityID}-${city.countryID}`}
-									value={city.cityID}
-								>
-									{city.cityName}
-								</Option>
-							))}
-						</Select>
-					</Col>
-				</Row>
-				<Divider>Map</Divider>
-				<Row>
-					<Col span={24} style={{ height: 400 }}>
-						<GoogleMap lat={latitude} lng={longitude} />
-					</Col>
-				</Row>
+				<Spin spinning={loading}>
+					<Row>
+						<Col span={8}>
+							<Select
+								showSearch
+								style={{ width: 200 }}
+								placeholder="Select Country"
+								optionFilterProp="children"
+								autoComplete="off"
+								onChange={this.countryChanged}
+								filterOption={(input, option) =>
+									option.children
+										.toLowerCase()
+										.indexOf(input.toLowerCase()) >= 0
+								}
+							>
+								{countries.map(country => (
+									<Option key={country.countryID}>
+										{country.countryName}
+									</Option>
+								))}
+							</Select>
+						</Col>
+						<Col span={8}>
+							<Select
+								showSearch
+								style={{ width: 200 }}
+								placeholder="Select State"
+								optionFilterProp="children"
+								onChange={this.stateChanged}
+								filterOption={(input, option) =>
+									option.children
+										.toLowerCase()
+										.indexOf(input.toLowerCase()) >= 0
+								}
+							>
+								{states.map(state => (
+									<Option key={state.stateID}>
+										{state.stateName}
+									</Option>
+								))}
+							</Select>
+						</Col>
+						<Col span={8}>
+							<Select
+								style={{ width: 200 }}
+								placeholder="Select City"
+								optionFilterProp="children"
+								onChange={this.citySelected}
+								filterOption={(input, option) =>
+									option.children
+										.toLowerCase()
+										.indexOf(input.toLowerCase()) >= 0
+								}
+							>
+								{cities.map(city => (
+									<Option
+										key={`${city.cityID}-${city.countryID}`}
+										value={city.cityID}
+									>
+										{city.cityName}
+									</Option>
+								))}
+							</Select>
+						</Col>
+					</Row>
+					<Divider>Map</Divider>
+					<Row>
+						<Col span={24} style={{ height: 400 }}>
+							<GoogleMap lat={latitude} lng={longitude} stations={stations} />
+						</Col>
+					</Row>
+				</Spin>
 			</React.Fragment>
 		);
 	}
